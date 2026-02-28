2e4cabb1580ae2739fb359f51d763f53ecbbbd61
Baltazzap <baltazzap@gmail.com>
2026-02-28 23:52:05 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
-from dotenv import load_dotenv
+from discord.ui import Button, View, Modal, TextInput  # ← ЭТОГО НЕ ХВАТАЛО!
+import asyncio
 import os
+from dotenv import load_dotenv
 
 # Загружаем переменные из .env
 load_dotenv()
